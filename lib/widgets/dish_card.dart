96d316a67ac423d155a5bf3d4da7e339cf6d4018
peppermint65 <chenyinjia2000@163.com>
2021-12-06 21:13:29 +0800

    cyj_is_sleepy4

---
+++ lib/widgets/dish_card.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'fav_button.dart';
+import 'package:delivery/dish_display.dart';
+
+class SuggestedDishCard extends StatelessWidget {
+  late var userID;
+  late var topFood;
+
+  SuggestedDishCard(this.topFood, this.userID);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/home/dishdisplay', arguments: PassDataDish(topFood, userID, []));
+      },
+      child: Container(
+        margin: const EdgeInsets.fromLTRB(16, 13, 16, 0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            renderCover(context),
+            renderCanteen(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget renderCanteen() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: Positioned(
+        left: 0,
+        right: 0,
+        bottom: 0,
+        height: 100,
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+          alignment: Alignment.bottomLeft,
+          child: Text(
+            topFood['foodStoreName'],
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget renderCover(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: Stack(
+        fit: StackFit.passthrough,
+        children: <Widget>[
+          Image.network(
+            topFood['foodUrl'],
+            height: 150,
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            height: 100,
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+              alignment: Alignment.bottomLeft,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.transparent, Colors.black54],
+                ),
+              ),
+              child: Text(
+                topFood['foodName'],
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            height: 100,
+            child: Container(
+              // padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+              alignment: Alignment.bottomRight,
+              child: FavButton(userID, topFood['foodID']),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/fav_button.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/cupertino.dart';
+import 'package:favorite_button/favorite_button.dart';
+import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+class _Global {
+  static int userID = 0;
+  static var foodID = 0;
+  static bool isFav = false;
+}
+
+class FavButton extends StatefulWidget {
+
+  FavButton(userID, foodID) {
+    _Global.userID = userID;
+    _Global.foodID = foodID;
+  }
+
+  @override
+  _FavButtonState createState() => _FavButtonState();
+}
+
+class _FavButtonState extends State<FavButton> {
+  @override
+  Widget build(BuildContext context) {
+    getIsFavorite(context);
+    return Container(
+      padding: const EdgeInsets.only(left: 5.0),
+      height: 40.0,
+      width: 40.0,
+      child: Center(
+          child: FavoriteButton(
+            isFavorite: _Global.isFav,
+            iconDisabledColor: Colors.white,
+            iconSize: 30.0,
+            valueChanged: (_isFavorite) {
+              if (_isFavorite) {
+                setFav(context);
+              } else {
+                unsetFav(context);
+              }
+            },
+          )
+      ),
+    );
+  }
+
+  setFav(BuildContext context) async {
+    var baseUrl = "http://47.110.160.49";
+    var uri = "/api/setStar/";
+    var body = {
+      'userID': _Global.userID,
+      'foodID': _Global.foodID,
+    };
+    http.Response response = await http.post(Uri.parse(baseUrl + uri), body: convert.jsonEncode(body));
+    final statusCode = response.statusCode;
+    final responseBody = response.body;
+    // print('[uri=$uri][statusCode=$statusCode][response=$responseBody]');
+  }
+
+  unsetFav(BuildContext context) async {
+    var baseUrl = "http://47.110.160.49";
+    var uri = "/api/unStar/";
+    var body = {
+      'userID': _Global.userID,
+      'foodID': _Global.foodID,
+    };
+    http.Response response = await http.post(Uri.parse(baseUrl + uri), body: convert.jsonEncode(body));
+    final statusCode = response.statusCode;
+    final responseBody = response.body;
+    // print('[uri=$uri][statusCode=$statusCode][response=$responseBody]');
+  }
+
+  getIsFavorite(BuildContext context) async {
+    var baseUrl = "http://47.110.160.49";
+    var uri = "/api/getEvaluateFood/";
+    var body = {
+      'userID': _Global.userID,
+      'foodID': _Global.foodID,
+    };
+    http.Response response = await http.post(Uri.parse(baseUrl + uri), body: convert.jsonEncode(body));
+    final statusCode = response.statusCode;
+    final responseBody = response.body;
+    var result = convert.jsonDecode(responseBody);
+    // print('[uri=$uri][statusCode=$statusCode][response=$responseBody]');
+
+    setState(() {
+      _Global.isFav = result['food']['hasStared'];
+    });
+    // print(isFav);
+  }
+}
